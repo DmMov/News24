43b741507aec6fda5ab2e7b542b6f08a4bfd09d1
Dm Mov <movchanyukd@gmail.com>
2019-11-12 18:50:28 +0200

    add auth buttonts

--- front/src/app/site-header/navigation-over/NavigationOver.jsx
+++ front/src/app/site-header/navigation-over/NavigationOver.jsx
@@ -3,12 +3,14 @@ import AlignSectionBox from 'app/align-section-box/AlignSectionBox';
 import NavigationMenu from './navigation-menu/NavigationMenu';
 
 import './NavigationOver.sass';
+import AuthBtns from './auth-status-panel/auth-btns/AuthBtns';
 
 const NavigationOver = () =>
   <section id="navigation-over">
     <AlignSectionBox>
       <div className="no-content-wrap">
         <NavigationMenu />
+        <AuthBtns />
       </div>
     </AlignSectionBox>
   </section>
---
+++ front/src/app/site-header/navigation-over/auth-status-panel/auth-btns/AuthBtns.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './AuthBtns.sass';
+
+const AuthBtns = () =>
+  <div id="auth-btns">
+    <button className="btn">реєстрація</button>
+    <button className="btn">вхід</button>
+  </div>
+
+export default AuthBtns;
