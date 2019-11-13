6613e4a5e562fe846d32af26913bede201367731
Dm Mov <movchanyukd@gmail.com>
2019-11-13 09:52:22 +0200

    add fake data

--- front/src/const.js
+++ front/src/const.js
@@ -1,1 +1,2 @@
 export * from 'constants/navigation-links';
+export * from 'constants/fake-data';
---
+++ front/src/constants/fake-data.js
@@ -0,0 +1,40 @@
+export const TAGS = [
+  {
+    id: '1',
+    title: "здоров'я",
+    createdAt: Date.now().toString()
+  },
+  {
+    id: '2',
+    title: 'кіно',
+    createdAt: Date.now().toString()
+  },
+  {
+    id: '3',
+    title: 'музика',
+    createdAt: Date.now().toString()
+  },
+  {
+    id: '4',
+    title: 'наука',
+    createdAt: Date.now().toString()
+  },
+  {
+    id: '5',
+    title: 'технології',
+    createdAt: Date.now().toString()
+  },
+  {
+    id: '6',
+    title: 'спорт',
+    createdAt: Date.now().toString()
+  },
+  {
+    id: '7',
+    title: 'мистецтво',
+    createdAt: Date.now().toString()
+  },
+];
+export const TAGS_TO_NEWS = [];
+export const NEWS = [];
+
